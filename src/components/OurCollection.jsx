dafe6cf85619b88e653cbbf3f8694f990549fc89
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-24 21:38:38 +0530

    loaded collection images

--- src/components/OurCollection.jsx
+++ src/components/OurCollection.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+import imageGranEspresso from "../assets/home/desktop/image-gran-espresso.png";
+import imagePlanalto from "../assets/home/desktop/image-planalto.png";
+import imagePiccollo from "../assets/home/desktop/image-piccollo.png";
+import imageDanche from "../assets/home/desktop/image-danche.png";
+
 const OurCollection = () => {
   return (
     <div className="mt-40">
@@ -10,6 +15,56 @@ const OurCollection = () => {
         >
           our collection
         </h1>
+      </div>
+      <div className="grid grid-cols-12 -mt-10">
+        <div className="col-span-3">
+          <div>
+            <img src={imageGranEspresso} alt="imageGranEspresso" />
+          </div>
+          <div>
+            <h2>Gran Espresso</h2>
+            <p>
+              Light and flavorful blend with cocoa and black pepper for an
+              intense experience
+            </p>
+          </div>
+        </div>
+        <div className="col-span-3">
+          <div>
+            <img src={imagePlanalto} alt="imagePlanalto" />
+          </div>
+          <div>
+            <h2>Planalto</h2>
+            <p>
+              Brazilian dark roast with rich and velvety body, and hints of
+              fruits and nuts
+            </p>
+          </div>
+        </div>
+        <div className="col-span-3">
+          <div>
+            <img src={imagePiccollo} alt="imagePiccollo" />
+          </div>
+          <div>
+            <h2>Piccollo</h2>
+            <p>
+              Mild and smooth blend featuring notes of toasted almond and dried
+              cherry
+            </p>
+          </div>
+        </div>
+        <div className="col-span-3">
+          <div>
+            <img src={imageDanche} alt="imageDanche" />
+          </div>
+          <div>
+            <h2>Danche</h2>
+            <p>
+              Ethiopian hand-harvested blend densely packed with vibrant fruit
+              notes
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
