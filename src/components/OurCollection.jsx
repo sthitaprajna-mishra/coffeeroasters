fafad092d92b3d007fb91ba753a824790d004126
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-24 22:01:54 +0530

    added text to collection responsively

--- src/components/OurCollection.jsx
+++ src/components/OurCollection.jsx
@@ -10,55 +10,59 @@ const OurCollection = () => {
     <div className="mt-40">
       <div className="text-center">
         <h1
-          className="text-2xl md:text-8xl lg:text-[10rem] font-fraunces tracking-wider font-black bg-clip-text 
+          className="text-4xl md:text-8xl lg:text-[10rem] font-fraunces tracking-wider font-black bg-clip-text 
         text-transparent bg-gradient-to-b from-grey to-lightCream border-1 border-red-500"
         >
           our collection
         </h1>
       </div>
-      <div className="grid grid-cols-12 -mt-10">
-        <div className="col-span-3">
+      <div className="grid grid-cols-12 mt-10 md:-mt-10">
+        <div className="col-span-12 space-y-6 md:space-y-12 mb-12 md:mb-0 md:col-span-3">
           <div>
-            <img src={imageGranEspresso} alt="imageGranEspresso" />
+            <img
+              className="mx-auto"
+              src={imageGranEspresso}
+              alt="imageGranEspresso"
+            />
           </div>
-          <div>
-            <h2>Gran Espresso</h2>
+          <div className="text-center space-y-6 w-4/5 mx-auto">
+            <h2 className="text-2xl font-bold font-fraunces">Gran Espresso</h2>
             <p>
               Light and flavorful blend with cocoa and black pepper for an
               intense experience
             </p>
           </div>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-12 space-y-6 md:space-y-12 mb-12 md:mb-0 md:col-span-3">
           <div>
-            <img src={imagePlanalto} alt="imagePlanalto" />
+            <img className="mx-auto" src={imagePlanalto} alt="imagePlanalto" />
           </div>
-          <div>
-            <h2>Planalto</h2>
+          <div className="text-center  space-y-6">
+            <h2 className="text-2xl font-bold font-fraunces">Planalto</h2>
             <p>
               Brazilian dark roast with rich and velvety body, and hints of
               fruits and nuts
             </p>
           </div>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-12 space-y-6 md:space-y-12 mb-12 md:mb-0 md:col-span-3">
           <div>
-            <img src={imagePiccollo} alt="imagePiccollo" />
+            <img className="mx-auto" src={imagePiccollo} alt="imagePiccollo" />
           </div>
-          <div>
-            <h2>Piccollo</h2>
-            <p>
+          <div className="text-center space-y-6 w-4/5 mx-auto">
+            <h2 className="text-2xl font-bold font-fraunces">Piccollo</h2>
+            <p className="font-barlow">
               Mild and smooth blend featuring notes of toasted almond and dried
               cherry
             </p>
           </div>
         </div>
-        <div className="col-span-3">
+        <div className="col-span-12 space-y-6 md:space-y-12 mb-12 md:mb-0 md:col-span-3">
           <div>
-            <img src={imageDanche} alt="imageDanche" />
+            <img className="mx-auto" src={imageDanche} alt="imageDanche" />
           </div>
-          <div>
-            <h2>Danche</h2>
+          <div className="text-center space-y-6 w-4/5 mx-auto">
+            <h2 className="text-2xl font-bold font-fraunces">Danche</h2>
             <p>
               Ethiopian hand-harvested blend densely packed with vibrant fruit
               notes
